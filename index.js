c10b64010506af92515a5634b348fc8d82430662
furqan_ashraf <furqan_ashraf@outlook.com>
2021-04-12 03:38:16 +0500

    fixed port problem

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post("/api/mine", (req, res) => {
 const DEFAULT_PORT = 3000;
 let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === "true") {
-  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() + 1000);
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
