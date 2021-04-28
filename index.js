1da67fbdbfb87115b7b7e17912cc55e20ba12981
furqan_ashraf <furqan_ashraf@outlook.com>
2021-04-28 05:23:44 +0500

    testing production

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ setTimeout(() => {
 app.use(express.json());
 
 app.get("/api/blocks", (req, res) => {
-  res.json(blockchain.chain);
+  res.send(blockchain.chain);
 });
 
 app.post("/api/mine", (req, res) => {
@@ -73,7 +73,7 @@ app.get("/api/mine-transactions", (req, res) => {
 
 app.get("/api/wallet-info", (req, res) => {
   const address = wallet.publicKey;
-  res.json({
+  res.send({
     address,
     balance: Wallet.calculateBalance({ chain: blockchain.chain, address }),
   });
@@ -105,7 +105,7 @@ let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === "true") {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
-const PORT =process.env.PORT|| PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
   if (PORT !== DEFAULT_PORT) {
