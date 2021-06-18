ea5e424669cf083c0c378f34c2682b4e87429ed5
Waleed ASHRAF <waleedashraf@outlook.com>
2021-06-19 01:54:55 +0200

    add config values

--- app/pubsub.js
+++ app/pubsub.js
@@ -2,11 +2,6 @@ const PubNub = require('pubnub')
 
 const config = require('../config')
 const credentials = config.PUBNUB
-// const CHANNELS = {
-//   TEST: 'TEST',
-//   TRANSACTION: 'TRANSACTION',
-//   BLOCK: 'BLOCK',
-// }
 class Pubsub {
   constructor({method, channels}) {
     this.pubnub = new PubNub(credentials)
@@ -34,17 +29,5 @@ class Pubsub {
       console.error(err)
     }
   }
-  // broadcastBlock({block}) {
-  //   this.publish({
-  //     channel: CHANNELS.BLOCKCHAIN,
-  //     message: JSON.stringify(block),
-  //   })
-  // }
-  // broadcastTransaction(transaction) {
-  //   this.publish({
-  //     channel: CHANNELS.TRANSACTION,
-  //     message: JSON.stringify(transaction),
-  //   })
-  // }
 }
 module.exports = Pubsub
--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 module.exports = {
+  isDevelopment,
+  STARTING_BALANCE: 0,
+  MINE_RATE: 3000,
   MINING_REWARD: 50,
   REWARD_ADDRESS: {address: '*authorized-reward*'},
   GENESIS_DATA: {
@@ -9,32 +14,25 @@ module.exports = {
     nonce: 0,
     data: [],
   },
-  MINE_RATE: 3000,
-  STARTING_BALANCE: 0,
   BANK_WALLET: {
-    privateKey: process.env.bank_wallet_private_key || '38d22e371e7176bcdf4281c4798e9c55457d438179940f19414d7d00780669d5',
-    publicKey: process.env.bank_wallet_public_key ||
-      '047c08ba522a3a3abc136611b13cad4ac422ba87226d85e7292f621dce2bb45b937b909dcb4eb5183f7b0b583f01f7a98d8b3f944976b720776a717f1a0a71a517',
+    privateKey: process.env.bank_wallet_private_key,
+    publicKey: process.env.bank_wallet_public_key,
   },
   DEFAULT_PORT: 3000,
-  isDevelopment: process.env.ENV === 'development',
   PUBNUB: {
     publishKey:
-      process.env.PUBLISH_KEY || 'pub-c-0cd2364e-dd3a-4632-b584-4bc1d23d68d2',
+      process.env.pubnub_publish_key,
     subscribeKey:
-      process.env.SUBSCRIBE_KEY || 'sub-c-8ce308c6-9ac0-11eb-8dfb-c2cb28a4a163',
+      process.env.pubnub_subscribe_key,
     secretKey:
-      process.env.SECRET_KEY ||
-      'sec-c-ZDdiNGEwMGMtMTJkYS00MWJjLWFmM2QtNTYzMDMzZmIwNmRk',
+      process.env.pubnub_secret_key,
   },
-  url: 'http://localhost:3000/blockchain',
-  APP: {
-    CHANNELS: ['unconfirmed-blocks'],
-  },
-  MINER: {
-    CHANNELS: ['transactions', 'confirmed-blocks'],
+  URL: isDevelopment ? 'http://localhost:3000' : 'https://coincrypto-app.herokuapp.com',
+  CHANNELS: {
+    APP: ['unconfirmed-blocks'],
+    MINER: ['transactions', 'confirmed-blocks'],
   },
   DATABASE: {
-    URL: 'mongodb+srv://admin:W5uvoBSmjFpM1faE@cluster0.lfwbr.mongodb.net/cryptochain?retryWrites=true&w=majority',
+    URL: process.env.database_url,
   },
 }
